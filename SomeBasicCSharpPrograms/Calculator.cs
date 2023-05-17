88204023eb2e9ba35b7f91c95f59c8fa66b4ab76
khushbu <khushbusoni475@gmail.com>
2023-05-17 21:51:56 +0530

    [khushbu] modify basic calculation of c# programs

---
+++ SomeBasicCSharpPrograms/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeBasicCSharpPrograms
+{
+    internal class Calculator
+    {
+        public void AddTwoNumber(int x, int y)
+        {
+            int sum = x + y;
+            Console.WriteLine($"sum: {sum}");
+        }
+        public void Substraction(int x, int y)
+        {
+            int diff = Math.Abs(x - y);
+            Console.WriteLine("Difference: {0}", diff);
+        }
+        public void Multiplication(int x, int y)
+        {
+            int multiplication = x * y;
+            Console.WriteLine("multiplication: {0}", multiplication);
+        }
+    }
+}
--- SomeBasicCSharpPrograms/Program.cs
+++ SomeBasicCSharpPrograms/Program.cs
@@ -11,6 +11,15 @@ namespace SomeBasicCSharpPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic c# programs");
+            Console.WriteLine("Wlcome to basic c# programs");
+            Calculator cal = new Calculator();
+            Console.WriteLine("please enter 1st number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter 2nd number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            cal.AddTwoNumber(x, y);
+            cal.Substraction(x, y);
+            cal.Multiplication(x, y);
             Console.ReadLine();
         }
     }
