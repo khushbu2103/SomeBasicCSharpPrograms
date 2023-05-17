02f26b1286d3fa9648187a49a36547b7183b8baf
khushbu <khushbusoni475@gmail.com>
2023-05-17 22:23:49 +0530

    [khushbu] modify reverse word,statement,number,palindrome number program

--- SomeBasicCSharpPrograms/Program.cs
+++ SomeBasicCSharpPrograms/Program.cs
@@ -10,8 +10,9 @@ namespace SomeBasicCSharpPrograms
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, World!");
             Console.WriteLine("please choose any one program from below option");
-            Console.WriteLine("1.Calculator\n2.FibonoccieSeries");
+            Console.WriteLine("1.Calculator\n2.FibonoccieSeries\n3.ReverseWord\n4.reverse each words inn same position\n5.reverse number.\n6.palindrom number");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -31,6 +32,29 @@ namespace SomeBasicCSharpPrograms
                     Console.WriteLine("please enter how many series you want to generate");
                     int n = Convert.ToInt32(Console.ReadLine());
                     FibonoccieSeries.Generatefibinoccienumb(n);
+                    break;
+                case 3:
+                    Console.WriteLine("please enter words");
+                    string Word = Console.ReadLine();
+                    Reverse.ReverseWord(Word);
+                    break;
+                case 4:
+                    Console.WriteLine("plese enter a statement");
+                    Word = Console.ReadLine();
+                    Reverse.ReverseEachWordInSamePosition(Word);
+                    break;
+                case 5:
+                    Console.WriteLine("please enter any number");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Reverse.ReverseNumber(x);
+                    break;
+                case 6:
+                    Console.WriteLine("please enter any number");
+                    x = Convert.ToInt32(Console.ReadLine());
+                    Reverse.Palindrome(x);
+                    break;
+                default:
+                    Console.WriteLine("please choose existing program");
                     break;
             }
             Console.ReadLine();
---
+++ SomeBasicCSharpPrograms/Reverse.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeBasicCSharpPrograms
+{
+    internal class Reverse
+    {
+        public static string ReverseWord(string Word)
+        {
+            String reverse = "";
+            foreach (char latter in Word)
+            {
+                reverse = latter + reverse;
+            }
+            //Console.Write(reverse);
+            return reverse;
+        }
+
+        public static void ReverseEachWordInSamePosition(string statement)
+        {
+            String reverse = "";
+            char[] delimeters = { ' ', ',', ':' };
+            string[] Words = statement.Split(delimeters);
+            foreach (string Word in Words)
+            {
+                string r = ReverseWord(Word);
+                reverse = reverse + r + " ";
+            }
+            Console.Write(reverse);
+        }
+
+        public static int ReverseNumber(int Number)
+        {
+            int Reminder = 0;
+            int reverse = 0;
+
+            while (Number != 0)
+            {
+                Reminder = Number % 10;
+                reverse = (reverse * 10) + Reminder;
+                Number = Number / 10;
+            }
+            return reverse;
+        }
+
+        public static void Palindrome(int Number)
+        {
+            int output = ReverseNumber(Number);
+            if (Number == output)
+            {
+                Console.WriteLine("{0} is palindrome:", Number);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not polindrome", Number);
+            }
+        }
+       
+    }
+}
